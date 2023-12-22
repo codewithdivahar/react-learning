58005963a6e96e43d8e7ae0bb56bdca2f96f3237
Divahar B <codewithdivahar@Divahars-MacBook-Pro.local>
2023-12-21 20:28:47 -0500

    Creating the application without create react app

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
-/*
-    <div id="parent">
-        <div id="child1">
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-        <div id="child2">
-            <h1>I'm h1 tag</h1>
-            <h2>I'm h2 tag</h2>
-        </div>
-    </div>
-*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
     "div", {id : "parent"},
     [ 
-    React.createElement("div", { id : "child1" }, 
+        React.createElement("div", { id : "child1" }, 
+            [
+                React.createElement("h1", { id : "heading1"}, "I'm h1 tag!"), 
+                React.createElement("h2", { id : "heading2"}, "I'm h2 tag!")
+            ]
+        ),React.createElement("div", { id : "child2" }, 
         [
-            React.createElement("h1", { id : "heading1"}, "I'm h1 tag!"), 
-            React.createElement("h2", { id : "heading2"}, "I'm h2 tag!")
+            React.createElement("h1", { id : "heading3"}, "I'm h1 tag!"), 
+            React.createElement("h2", { id : "heading4"}, "I'm h2 tag!")
         ]
-    ),React.createElement("div", { id : "child2" }, 
-    [
-        React.createElement("h1", { id : "heading3"}, "I'm h1 tag!"), 
-        React.createElement("h2", { id : "heading4"}, "I'm h2 tag!")
-    ]
 )]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React Learnings 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- Auto Refresh the page (HMR) - Hot Module Replacement
+- Parcel Uses File Watching Algorithm -> written in C++
+- Parcel is Caching for the faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Content Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- Diagnostic
+- Better Error Handling or Error Suggestions
+- Host your app on https
+- Tree shaking - remove unused code
+- Different Dev and Production bundles
